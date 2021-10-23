4c836ba1dbcb445f36f8a9113422bb94546b80e5
Bartosz Jankowski <bjankowski333@gmail.com>
2021-10-23 21:07:08 +0200

    Fixed spacing in scripts

--- CubeEx2.cs
+++ CubeEx2.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class CubeEx2 : MonoBehaviour
 {
     
-	public float speed = 10.0f;
-	private float start_position;
-	private bool odwroc;
-	private float reverse_position;
-	Rigidbody rb;
+    public float speed = 10.0f;
+    private float start_position;
+    private bool odwroc;
+    private float reverse_position;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
 
-		rb = GetComponent<Rigidbody>();
-		start_position = transform.position.x - speed;
-		reverse_position = start_position + speed;
+	rb = GetComponent<Rigidbody>();
+	start_position = transform.position.x - speed;
+	reverse_position = start_position + speed;
 
     }
 
@@ -27,22 +27,22 @@ public class CubeEx2 : MonoBehaviour
 
     }
 
-	void FixedUpdate()
+    void FixedUpdate()
+    {
+	Vector3 velocity = new Vector3(-1, 0, 0);
+	velocity = velocity.normalized * speed * Time.deltaTime;
+	if (transform.position.x > start_position && odwroc == false)
 	{
-		Vector3 velocity = new Vector3(-1, 0, 0);
-		velocity = velocity.normalized * speed * Time.deltaTime;
-		if (transform.position.x > start_position && odwroc == false)
-		{
-			rb.MovePosition(transform.position + velocity);
-		}
-		else if (transform.position.x < reverse_position && odwroc == true)
-		{
-			rb.MovePosition(transform.position - velocity);
-		}
-		
-		if (transform.position.x <= start_position)
-			odwroc = true;
-		if (transform.position.x >= reverse_position)
-			odwroc = false;
+		rb.MovePosition(transform.position + velocity);
 	}
+	else if (transform.position.x < reverse_position && odwroc == true)
+	{
+		rb.MovePosition(transform.position - velocity);
+	}
+		
+	if (transform.position.x <= start_position)
+		odwroc = true;
+	if (transform.position.x >= reverse_position)
+		odwroc = false;
+    }
 }
